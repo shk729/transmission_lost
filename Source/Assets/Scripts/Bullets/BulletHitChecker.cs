5460c8926d5a9893763b1c7ff580a52c71b6aac8
BrutuS1k <petr.lagun@gmail.com>
2018-01-27 02:19:24 +0300

    monster+bullets hit

---
+++ Source/Assets/Scripts/Bullets/BulletHitChecker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHitChecker : MonoBehaviour 
+{
+
+	void OnTriggerEnter2D(Collider2D target)
+	{
+
+		if (target.gameObject.tag == "Monster") 
+		{
+			
+			target.gameObject.SetActive (false);
+		}
+	}
+
+
+}
--- Source/Assets/Scripts/Player/PlayerShoot.cs
+++ Source/Assets/Scripts/Player/PlayerShoot.cs
@@ -29,28 +29,23 @@ public class PlayerShoot : MonoBehaviour {
 
 	void Fire()
 	{
-		bulletPosition = transform.position;
+
 
 		if (Time.time>(lastShotTime + shootSpeed))
 		{
-			GameObject bulletClone =  Instantiate(bullet, bulletPosition, transform.rotation); 
-			bulletClone.gameObject.SetActive (true);
-
 			Vector2 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector2 mouseDirection = new Vector2 (currentMousePosition.x - transform.position.x,
 				currentMousePosition.y - transform.position.y );
 
+			bulletPosition.x = bullet.transform.position.x;
+			bulletPosition.y = bullet.transform.position.y;
 
-			Debug.Log (currentMousePosition +" " +transform.position + " "+ mouseDirection.x+ " "+ mouseDirection.y);
 
-
-			//Physics.IgnoreCollision(bulletClone.collider, collider);
-			//bulletClone.GetComponent<Rigidbody2D>().velocity = bullet.transform.forward * 6;
+			GameObject bulletClone =  Instantiate(bullet, bulletPosition, transform.rotation); 
+			bulletClone.gameObject.SetActive (true);
 
 			bulletClone.GetComponent<Rigidbody2D>().AddForce(mouseDirection.normalized * bulletSpeed);
 			Destroy(bulletClone, 10.0f);
-			//Rigidbody2D rb = bulletClone.GetComponent<Rigidbody2D> ();
-			//rb.AddForce(transform.forward*bulletImpulse, ForceMode2D.Impulse);
 
 			lastShotTime = Time.time;
 		}
