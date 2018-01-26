56a725eec1954f05290c5c025c597c7577bd42e8
BrutuS1k <petr.lagun@gmail.com>
2018-01-26 23:46:21 +0300

    move to source folder

---
+++ Source/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	private GameObject player;
+	static Rigidbody2D playerBody;
+
+	Quaternion newRotation;
+	float moveAngle;                                                      //angle in degrees
+	float maxVelocity = 3.5f;
+
+	Vector2 moveDirection;
+	Vector2 currentMousePosition;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		playerBody = GetComponent<Rigidbody2D>();
+		player = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		KeyboardMove();
+		MouseDirection ();
+
+	}
+
+	void MouseDirection()
+	{
+		currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 mouseDirection = new Vector2 (currentMousePosition.x - transform.position.x,
+			currentMousePosition.y - transform.position.y );
+		Debug.DrawRay(transform.position, mouseDirection, Color.red);
+
+
+		//rotation calculation
+		float rotationSpeed = 10f;
+		playerBody.freezeRotation = true;
+
+		Vector3 eulerAngles = player.transform.rotation.eulerAngles;
+		float playerAngle = eulerAngles.z;
+		if (playerAngle > 180)
+			playerAngle -= 360;
+		float rotAngle = Mathf.Atan (currentMousePosition.normalized.y / currentMousePosition.normalized.x);
+
+		if (currentMousePosition.normalized.x >= 0)
+			moveAngle = rotAngle * Mathf.Rad2Deg - 90;
+		else if (currentMousePosition.normalized.x < 0)
+			moveAngle = rotAngle * Mathf.Rad2Deg + 90;
+
+		newRotation = Quaternion.Euler (0, 0, moveAngle);
+		player.transform.rotation = Quaternion.Slerp (player.transform.rotation, newRotation, Time.fixedDeltaTime * rotationSpeed);
+
+		playerBody.freezeRotation = false;
+
+
+	}
+
+	void KeyboardMove()
+	{
+		float forceScale = 5f;
+
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		if (inputX != 0 || inputY != 0)
+		{
+			moveDirection = new Vector2(inputX * forceScale, inputY * forceScale);
+			//PlayerMoveCalculation(moveDirection);
+		}
+
+		playerBody.AddForce (moveDirection * 1.5f);
+	} //void KeyboardMove
+
+	public static Vector2 GetPlayerVelocity()
+	{
+		return playerBody.velocity;
+	}
+		
+}
---
+++ Source/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+	//[SerializeField]
+	public GameObject bullet;
+
+	private Transform bulletPosition;
+
+	public float bulletImpulse = 300f;
+	public float shootSpeed = 1;
+	public float lastShotTime;
+
+	void Start() {
+		lastShotTime = 0;
+	}
+
+	void FixedUpdate () 
+	{
+		if (Input.GetKey(KeyCode.Mouse0))
+		{
+			Fire ();
+		}
+	}//	void FixedUpdate ()
+
+
+	void Fire()
+	{
+		if (Time.time>(lastShotTime + shootSpeed))
+		{
+			GameObject bulletClone =  Instantiate(bullet, bulletPosition, transform.rotation); 
+
+			//Physics.IgnoreCollision(bulletClone.collider, collider);
+
+			Rigidbody2D rb = bulletClone.GetComponent<Rigidbody2D> ();
+			rb.AddForce(transform.forward*bulletImpulse, ForceMode2D.Impulse);
+
+			lastShotTime = Time.time;
+		}
+
+
+	}
+}
