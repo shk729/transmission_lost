9389e99d53f26227c23d8924f67c69745530f38a
BrutuS1k <petr.lagun@gmail.com>
2018-01-27 14:36:24 +0300

    crosshair

---
+++ Source/Assets/Scripts/HUD/Cursor/ChangeCursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour {
+
+	public Texture2D mouseTexture;
+
+	void Awake () {
+		Cursor.visible = false;
+		//Screen.showCursor=false;
+	}
+	void OnGUI () {
+		Vector2 m = Event.current.mousePosition;
+		GUI.depth=0;
+		GUI.Label(new Rect(m.x,m.y,mouseTexture.width,mouseTexture.height),mouseTexture);
+	}
+}
