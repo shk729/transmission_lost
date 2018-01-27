56237b15dde41a46e77f61ec4b9c588a92544fe8
BrutuS1k <petr.lagun@gmail.com>
2018-01-28 00:46:54 +0300

    HP bars

---
+++ Source2/Assets/Scripts/Stantion/StantionHP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StantionHP : MonoBehaviour {
+
+	[SerializeField]
+	public Slider stantionHPbar;
+
+	public float curValue_slider;
+	private float maxValue_slider = 2000f;
+
+	void Start()
+	{
+		playerHPbar_initialization ();
+	}
+
+	void playerHPbar_initialization ()
+	{  
+		curValue_slider = maxValue_slider;
+		stantionHPbar.minValue = 0;
+		stantionHPbar.maxValue = maxValue_slider;
+		stantionHPbar.value = curValue_slider;
+
+	}
+}
