d4e91d81284baf1dd2c2185a79f64d51741490f2
BrutuS1k <petr.lagun@gmail.com>
2018-01-27 14:06:48 +0300

    effects of die and bullet hit

--- Source/Assets/Scripts/Bullets/BulletHitChecker.cs
+++ Source/Assets/Scripts/Bullets/BulletHitChecker.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class BulletHitChecker : MonoBehaviour 
 {
+	[SerializeField]
+	private ParticleSystem hitEffect;
+
+	[SerializeField]
+	private ParticleSystem monsterDie;
+
 
 	void OnTriggerEnter2D(Collider2D target)
 	{
 		
 		if (target.gameObject.tag == "Monster") 
 		{
-			
+			/*
+			//exlosion animation
+			ParticleSystem hitEffect_clone =  Instantiate(hitEffect, transform.position, transform.rotation); 
+			hitEffect_clone.gameObject.SetActive (true);
+			Destroy (hitEffect_clone, hitEffect.duration);
+			*/
+
+			//monster die animation
+			ParticleSystem monsterDie_clone =  Instantiate(monsterDie, target.transform.position, target.transform.rotation); 
+			monsterDie_clone.gameObject.SetActive (true);
+			Destroy (monsterDie_clone, monsterDie.duration);
 			target.gameObject.SetActive (false);
 		}
 
@@ -18,16 +34,21 @@ public class BulletHitChecker : MonoBehaviour
 
 	void OnCollisionEnter2D(Collision2D target)
 	{
-		if (target.gameObject.tag == "Player") 
-		{
+		if (target.gameObject.tag == "Player") {
 			
-		}
-		else if (target.gameObject.tag == "Monster") 
-		{
+		} else if (target.gameObject.tag == "Monster") {
 
-		}
 
-		else Destroy (this.gameObject, 0f);
+
+		} else {
+
+			//exlosion animation
+			ParticleSystem hitEffect_clone =  Instantiate(hitEffect, transform.position, transform.rotation); 
+			hitEffect_clone.gameObject.SetActive (true);
+			Destroy (hitEffect_clone, 0.8f);
+
+			Destroy (this.gameObject, 0f);
+		}
 	}
 
 }
