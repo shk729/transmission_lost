6e545987f52febd67459f72ace76b836fcdf16d5
shk729 <takeshark729@gmail.com>
2018-01-27 11:07:00 +0300

    Update scenes location

---
+++ Source/Assets/Scripts/Exit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#elif UNITY_WEBPLAYER
+		//Application.OpenURL(webplayerQuitURL);
+		#else
+		Application.Quit();
+		#endif	
+	}
+}
