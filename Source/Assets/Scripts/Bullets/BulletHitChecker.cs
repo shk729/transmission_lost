963e5c1af78f248c86167f64a60279f81490abb9
BrutuS1k <petr.lagun@gmail.com>
2018-01-27 12:27:42 +0300

    POI progress process

--- Source/Assets/Scripts/Bullets/BulletHitChecker.cs
+++ Source/Assets/Scripts/Bullets/BulletHitChecker.cs
@@ -7,13 +7,27 @@ public class BulletHitChecker : MonoBehaviour
 
 	void OnTriggerEnter2D(Collider2D target)
 	{
-
+		
 		if (target.gameObject.tag == "Monster") 
 		{
 			
 			target.gameObject.SetActive (false);
 		}
+
 	}
 
+	void OnCollisionEnter2D(Collision2D target)
+	{
+		if (target.gameObject.tag == "Player") 
+		{
+			
+		}
+		else if (target.gameObject.tag == "Monster") 
+		{
+
+		}
+
+		else Destroy (this.gameObject, 0f);
+	}
 
 }
