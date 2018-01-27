c76410acc6f5e5736b681ebff68403ddb54ad5ae
BrutuS1k <petr.lagun@gmail.com>
2018-01-28 01:41:13 +0300

    arrow

---
+++ Source2/Assets/Scripts/Player/DirectionArrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionArrow : MonoBehaviour {
+
+	private Transform arrow;
+	private Vector3 destination;
+
+	void Start ()
+	{		
+		arrow = GetComponent<Transform> ();
+	}
+
+	void FixedUpdate () {
+		//destination = GameObject.Find("Retranslator").transform.position;
+
+		Vector3 playerPosition = PlayerMove.GetPlayerPosition();
+		Vector2 moveDirection  = new Vector2(destination.x-playerPosition.x, destination.y-playerPosition.y);
+
+		float moveAngle = 0;
+		float rotAngle = Mathf.Atan(moveDirection.normalized.y/moveDirection.normalized.x); 
+
+		if (moveDirection.normalized.x>=0)
+			moveAngle = rotAngle * Mathf.Rad2Deg-90;
+		else if (moveDirection.normalized.x<0)
+			moveAngle = rotAngle * Mathf.Rad2Deg+90;
+
+		arrow.rotation = Quaternion.Euler (0, 0, moveAngle+180);
+	}
+
+
+	public void SetObjjectToDirection(GameObject target)
+	{
+		destination = target.transform.position;
+	}
+}
