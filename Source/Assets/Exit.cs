fd136bffa69957d025756cbd69d4172bc18e6c58
shk729 <takeshark729@gmail.com>
2018-01-27 04:34:10 +0300

    Menu_dummy

---
+++ Source/Assets/Exit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Application.Quit ();	
+	}
+}
